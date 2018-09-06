dfc4e27c9b8e52b1644542ea4f823c33e843bc46
Neelam Saxena <neelam.saxena@cgt.co.in>
2018-09-06 11:13:55 +0530

    Project Set Up

---
+++ app/src/main/java/com/assignment/application/AssignmentApplication.java
@@ -0,0 +1,28 @@
+package com.assignment.application;
+
+import android.app.Activity;
+import android.app.Application;
+
+import javax.inject.Inject;
+
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+
+/**
+ * Created by Neelam Saxena on 6/9/18.
+ */
+public class AssignmentApplication extends Application implements HasActivityInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> activityDispatchingAndroidInjector;
+
+    @Override
+    public DispatchingAndroidInjector<Activity> activityInjector() {
+        return activityDispatchingAndroidInjector;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
